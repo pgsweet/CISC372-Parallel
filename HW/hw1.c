3c45e95c9d6eb2173b10d76bb204c623d7ed0e6d
pgsweet <pgsweet@udel.edu>
2025-02-19 21:47:19 -0500

    finsihed part 2

--- HW/hw1.c
+++ HW/hw1.c
@@ -38,10 +38,45 @@ void matrixArrayOfArrays(int m, int n) {
     free(matrix);
 }
 
+void matrixOneBigArray(int m, int n) {
+    // Part A
+    float **matrix = (float**)malloc(m*sizeof(float*));
+    matrix[0] = (float*)malloc(m*n*sizeof(float));
+    // Part B
+    for (int i = 0; i < m; i++) {
+        matrix[i] = matrix[0] + i*n;
+    }
+    // Part C
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            matrix[i][j] = (float)(i*n) + j + 1;
+        }
+    }
+    // Part D
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%f\t", matrix[i][j]);
+        }
+        printf("\n");
+    }
+    // Part E
+    printf("\n");
+    for (int j = 0; j < n; j++) {
+        for (int i = 0; i < m; i++) {
+            printf("%f\t", matrix[i][j]);
+        }
+        printf("\n");
+    }
+    // Part F
+    free(matrix[0]);
+    free(matrix);
+}
+
 
 int main(int argc,char** argv){
     matrixArrayOfArrays(M,N);
-    // matrixOneBigArray(M,N);
+    printf("\n\n");
+    matrixOneBigArray(M,N);
 
     return 0;
 }
